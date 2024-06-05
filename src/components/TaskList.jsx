a593397016e6d1f943e1531420e1334673ec8b98
Aman kumar <amank0259@gmail.com>
2024-06-05 17:53:25 +0530

    connect the task manager to redux

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { CiEdit } from "react-icons/ci";
 import { MdDeleteSweep } from "react-icons/md";
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteTask, editTask } from '../redux/reducer';
 
 
-const TaskList = ({ tasks, deleteTask, editTask }) => {
+const TaskList = () => {
+    const tasks = useSelector((state) => state.tasks.tasks) //get task from redux store
+    const dispatch = useDispatch();
+
+    // handle task delete action by redux dispatch
+    const handleDeleteTask = (index) => {
+        dispatch(deleteTask(index));
+    }
+    // handle task edit action by redux dispatch
+    const handleEditTask = (index) => {
+        const newTask = prompt("Edit the task:", tasks[index]);
+        if (newTask !== null) {
+            dispatch(editTask({ index, newTask }));
+        }
+    };
     return (
         <div className='md:px-64 mx-auto py-5'>
             <ul className=''>
@@ -11,8 +27,8 @@ const TaskList = ({ tasks, deleteTask, editTask }) => {
                     <li className='flex flex-row justify-between px-2 py-4 shadow-xl my-2 rounded-lg items-center' key={index}>
                         {task}
                         <div className='flex flex-row justify-between gap-4'>
-                            <button className='' onClick={() => editTask(index)}><CiEdit size={30} /></button>
-                            <button className='' onClick={() => deleteTask(index)}><MdDeleteSweep size={30} /></button>
+                            <button className='' onClick={() => handleEditTask(index)}><CiEdit size={30} /></button>
+                            <button className='' onClick={() => handleDeleteTask(index)}><MdDeleteSweep size={30} /></button>
                         </div>
                     </li>
                 ))}
