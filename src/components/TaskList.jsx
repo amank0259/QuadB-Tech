dffddb671c23bbe7e9d9d42f0e2cb0099348d77f
Aman kumar <amank0259@gmail.com>
2024-06-05 15:54:24 +0530

    added the task manager component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-function TaskList() {
+const TaskList = ({ tasks, deleteTask, editTask }) => {
     return (
-        <div className='px-64 mx-auto my-5'>TaskList</div>
-    )
-}
+        <div className='px-64 mx-auto my-5'>
+            <ul className='list-none'>
+                {tasks.map((task, index) => (
+                    <li className='mx-3' key={index}>
+                        {task}
+                        <button className='ml-3' onClick={() => editTask(index)}>Edit</button>
+                        <button className='ml-3' onClick={() => deleteTask(index)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-export default TaskList
+export default TaskList;
