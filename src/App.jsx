0de27e37a58640f6dc3c595230054f085996920c
Aman kumar <amank0259@gmail.com>
2024-06-05 14:49:18 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center text-xl md:text-4xl font-semibold'>TO-Do List</h1>
+
+    </div>
+  )
+}
+
+export default App
